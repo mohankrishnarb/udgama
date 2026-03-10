02d342bdfc0594e8705f62c5333597d0d1518f7e
Mohan Krishna <mohans@Mohans-MacBook-Pro.local>
2026-03-10 18:15:32 +0530

    initial commit

---
+++ src/app/layouts/layouts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layouts',
+  imports: [],
+  templateUrl: './layouts.html',
+  styleUrl: './layouts.scss',
+})
+export class Layouts {
+
+}
